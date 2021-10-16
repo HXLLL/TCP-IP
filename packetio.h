84716ad44118e17117e68ae3b65495dd41864502
HXLLL <1150189655@qq.com>
2021-10-16 16:18:03 +0800

    format comment

--- packetio.h
+++ packetio.h
@@ -1,6 +1,6 @@
 /*********************
- * @file packetio .h
- * @brief Library supporting sending/receiving Ethernet II frames .
+ * @file packetio.h
+ * @brief Library supporting sending/receiving Ethernet II frames.
  */
 
 #ifndef PACKETIO_H
@@ -15,12 +15,12 @@
 /**
 * @brief Encapsulate some data into an Ethernet II frame and send it.
 * *
-* @param buf Pointer to the payload .
-* @param len Length of the payload .
-* @param ethtype EtherType field value of this frame .
-* @param destmac MAC address of the destination .
+* @param buf Pointer to the payload.
+* @param len Length of the payload.
+* @param ethtype EtherType field value of this frame.
+* @param destmac MAC address of the destination.
 * @param id ID of the device ( returned by ‘addDevice ‘) to send on.
-* @return 0 on success , -1 on error .
+* @return 0 on success, -1 on error.
 * @see addDevice
 */
 int sendFrame(const void *buf, int len, int ethtype,
@@ -29,21 +29,21 @@ int sendFrame(const void *buf, int len, int ethtype,
 /**
 * @brief Process a frame upon receiving it.
 * *
-* @param buf Pointer to the frame .
-* @param len Length of the frame .
+* @param buf Pointer to the frame.
+* @param len Length of the frame.
 * @param id ID of the device ( returned by ‘addDevice ‘) receiving
-* current frame .
-* @return 0 on success , -1 on error .
+* current frame.
+* @return 0 on success, -1 on error.
 * @see addDevice
 */
 typedef int (*frameReceiveCallback)(const void *, int, int);
 
 /**
 * @brief Register a callback function to be called each time an
-* Ethernet II frame was received .
+* Ethernet II frame was received.
 * *
-* @param callback the callback function .
-* @return 0 on success , -1 on error .
+* @param callback the callback function.
+* @return 0 on success, -1 on error.
 * @see frameReceiveCallback
 */
 int setFrameReceiveCallback(frameReceiveCallback callback);
